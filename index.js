ae78f440832cc182a8dbaad84df4a806773395ff
Osmar Domingos <osdomingos@gmail.com>
2025-03-20 08:12:50 -0300

    5 try

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -22,18 +25,23 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.post('/api/shorturl', (req, res) => {
+  shortUrl++;
+
+  console.log("Recebido:", req.body);
+
+  let url = new URL(req.body.url).hostname;
+  
+  dns.lookup(url, (err, address) => {
+    if (err) {
+      return res.json({ error: 'invalid url' });
+    }
+
+    res.json({ original_url: req.body.url, short_url: shortUrl });
+  });
+});
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
-
-app.post('/api/shorturl', (req, res, next) => {
-  shortUrl++;
-
-  dns.lookup(req.body.url, (err, url) => {
-    if (err) return res.json({ error: 'invalid url' });
-
-    res.json({ original_url: req.body.url, short_url: 1})
-  })
-});
-
 
